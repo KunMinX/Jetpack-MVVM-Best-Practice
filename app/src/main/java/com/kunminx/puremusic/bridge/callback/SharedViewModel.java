f7fac5071611d46a8913ec5345fe824d55c78393
KunMinX <kunminx@gmail.com>
2019-11-06 23:33:41 +0800

    update comment

--- app/src/main/java/com/kunminx/puremusic/bridge/callback/SharedViewModel.java
+++ app/src/main/java/com/kunminx/puremusic/bridge/callback/SharedViewModel.java
@@ -30,6 +30,9 @@ public class SharedViewModel extends ViewModel {
 
     // TODO tip 1：此处演示通过 UnPeekLiveData 配合 SharedViewModel 来发送 生命周期安全的、事件源可追溯的 通知。
 
+    // 并且，使用 Application 级的 SharedViewModel，而不是单例，来负责 全局的页面消息通信，是为了 约束作用域，
+    // 以免视图控制器间的消息 污染到 视图控制器之外的领域。
+
     // 如果这么说还不理解的话，
     // 详见 https://xiaozhuanlan.com/topic/0168753249 和 https://xiaozhuanlan.com/topic/6257931840
 
