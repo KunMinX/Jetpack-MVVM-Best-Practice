f8ba8f93a38be19dd3d8a5dbf88894d0901be52a
KunMinX <kunminx@gmail.com>
2020-03-13 11:31:38 +0800

    move commonBinding back to app module, because it is often be modified.

--- app/src/main/java/com/kunminx/puremusic/ui/base/binding/CommonBinding.java
+++ app/src/main/java/com/kunminx/puremusic/ui/base/binding/CommonBinding.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.kunminx.architecture.ui.binding;
+package com.kunminx.puremusic.ui.base.binding;
 
 import android.graphics.drawable.Drawable;
 import android.view.View;
