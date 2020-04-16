601803cd79bfcd4fc7e21d65a1cc4ea83d5589ca
KunMinX <kunminx@gmail.com>
2020-04-16 16:05:33 +0800

    sharedViewModel tip

--- app/src/main/java/com/kunminx/puremusic/bridge/callback/SharedViewModel.java
+++ app/src/main/java/com/kunminx/puremusic/bridge/callback/SharedViewModel.java
@@ -32,7 +32,6 @@ import java.util.List;
  * 如果这样说还不理解的话，详见 https://xiaozhuanlan.com/topic/6257931840
  *
  *
- *
  * Create by KunMinX at 19/10/16
  */
 public class SharedViewModel extends ViewModel {
@@ -40,7 +39,7 @@ public class SharedViewModel extends ViewModel {
     // TODO tip 1：此处演示通过 UnPeekLiveData 配合 SharedViewModel 来发送 生命周期安全的、事件源可追溯的 通知。
 
     //TODO tip 2：并且，在页面通信的场景下，使用全局 ViewModel，是因为它被封装在 base 页面中，避免页面之外的组件拿到，从而造成不可预期的推送，
-    // 而且尽可能使用单例或 vm 托管 liveData，这样调试时能根据内存中的 liveData 对象找到事件源，
+    // 而且尽可能使用单例或 ViewModel 托管 liveData，这样调试时能根据内存中的 liveData 对象找到事件源，
     // liveDataBus 这种通过 tag 来标记的，难以找到。
 
     // 如果这么说还不理解的话，
