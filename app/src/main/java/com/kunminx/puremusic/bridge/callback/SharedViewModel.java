e3ad5078a371ea9906d707303b646d8fb54aa026
KunMinX <kunminx@gmail.com>
2020-04-16 16:00:39 +0800

    sharedViewModel tip

--- app/src/main/java/com/kunminx/puremusic/bridge/callback/SharedViewModel.java
+++ app/src/main/java/com/kunminx/puremusic/bridge/callback/SharedViewModel.java
@@ -25,11 +25,12 @@ import java.util.List;
 
 /**
  *
- * TODO tip：callbackViewModel 的职责仅限于 页面通信，不建议在此处理 UI 逻辑，
+ * TODO tip：callback - ViewModel 的职责仅限于 页面通信，不建议在此处理 UI 逻辑，
  * UI 逻辑只适合在 Activity/Fragment 等视图控制器中完成，是 “数据驱动” 的一部分，
  * 将来升级到 Jetpack Compose 更是如此。
  *
  * 如果这样说还不理解的话，详见 https://xiaozhuanlan.com/topic/6257931840
+ *
  *
  *
  * Create by KunMinX at 19/10/16
@@ -38,8 +39,9 @@ public class SharedViewModel extends ViewModel {
 
     // TODO tip 1：此处演示通过 UnPeekLiveData 配合 SharedViewModel 来发送 生命周期安全的、事件源可追溯的 通知。
 
-    // 并且，使用 Application 级的 SharedViewModel，而不是单例，来负责 全局的页面消息通信，是为了 约束作用域，
-    // 以免视图控制器间的消息 污染到 视图控制器之外的领域。
+    //TODO tip 2：并且，在页面通信的场景下，使用全局 ViewModel，是因为它被封装在 base 页面中，避免页面之外的组件拿到，从而造成不可预期的推送，
+    // 而且尽可能使用单例或 vm 托管 liveData，这样调试时能根据内存中的 liveData 对象找到事件源，
+    // liveDataBus 这种通过 tag 来标记的，难以找到。
 
     // 如果这么说还不理解的话，
     // 详见 https://xiaozhuanlan.com/topic/0168753249 和 https://xiaozhuanlan.com/topic/6257931840
