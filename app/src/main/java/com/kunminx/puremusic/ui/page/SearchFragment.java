3e21a2d98a1efb3c8f94c405b5f57999a54d5bce
KunMinX <kunminx@gmail.com>
2020-04-27 15:54:23 +0800

    update copyright info

--- app/src/main/java/com/kunminx/puremusic/ui/page/SearchFragment.java
+++ app/src/main/java/com/kunminx/puremusic/ui/page/SearchFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 KunMinX
+ * Copyright 2018-2020 KunMinX
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
