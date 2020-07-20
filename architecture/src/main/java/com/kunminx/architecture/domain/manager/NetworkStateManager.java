17c5dde23acaf0fdea6d60edf5e215bdf11ceb54
KunMinX <kunminx@gmail.com>
2020-07-20 22:57:21 +0800

    minus NetworkStateManager function

--- architecture/src/main/java/com/kunminx/architecture/domain/manager/NetworkStateManager.java
+++ architecture/src/main/java/com/kunminx/architecture/domain/manager/NetworkStateManager.java
@@ -23,8 +23,6 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.DefaultLifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
 
-import com.kunminx.architecture.data.repository.DataResult;
-import com.kunminx.architecture.ui.callback.UnPeekLiveData;
 import com.kunminx.architecture.utils.Utils;
 
 /**
@@ -33,7 +31,6 @@ import com.kunminx.architecture.utils.Utils;
 public class NetworkStateManager implements DefaultLifecycleObserver {
 
     private static final NetworkStateManager S_MANAGER = new NetworkStateManager();
-    public final DataResult<NetState> networkStateCallback = new DataResult<>();
     private NetworkStateReceive mNetworkStateReceive;
 
     private NetworkStateManager() {
