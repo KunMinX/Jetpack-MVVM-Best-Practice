f1ef73666478ed8d77bded37efffee9a5d76fea4
KunMinX <kunminx@gmail.com>
2020-07-21 11:35:21 +0800

    update comment

--- architecture/src/main/java/com/kunminx/architecture/domain/request/BaseRequest.java
+++ architecture/src/main/java/com/kunminx/architecture/domain/request/BaseRequest.java
@@ -29,6 +29,11 @@ public class BaseRequest {
 
     protected final UnPeekLiveData<NetState> netStateEvent = new UnPeekLiveData<>();
 
+    //TODO tip 向 ui 层提供的 request LiveData，使用抽象的 ProtectedUnPeekLiveData 而不是 UnPeekLiveData
+    // 如此是为了来自数据层的数据，在 ui 层中只读，以避免团队新手不可预期的误用
+
+    // 如果这样说还不理解的话，详见 https://xiaozhuanlan.com/topic/6719328450 文末加餐的解析
+
     public ProtectedUnPeekLiveData<NetState> getNetStateEvent() {
         return netStateEvent;
     }
