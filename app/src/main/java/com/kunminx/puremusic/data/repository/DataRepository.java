be32a2b269540afc29dff2dbe89a8747cbb84e95
KunMinX <kunminx@gmail.com>
2020-09-23 18:28:17 +0800

    update comment

--- app/src/main/java/com/kunminx/puremusic/data/repository/DataRepository.java
+++ app/src/main/java/com/kunminx/puremusic/data/repository/DataRepository.java
@@ -59,6 +59,17 @@ public class DataRepository implements ILocalSource, IRemoteSource {
         return responseCodeLiveData;
     }
 
+    /**
+     * TODO: 建议在 DataRepository 使用 DataResult 而非 LiveData 来返回结果：
+     * liveData 是专用于页面开发的、用于解决生命周期安全问题的组件，
+     * 有时候数据并非一定是通过 liveData 来分发给页面，也可能是通过别的组件去通知给非页面的东西，
+     * 这时候 repo 方法中内定通过 liveData 分发就不太合适，不如一开始就规定不在数据层通过 liveData 返回结果。
+     * <p>
+     * 如果这样说还不理解的话，详见《如何让同事爱上架构模式、少写 bug 多注释？》篇的解析
+     * https://xiaozhuanlan.com/topic/8204519736
+     *
+     * @param result
+     */
     @Override
     public void getFreeMusic(DataResult<TestAlbum> result) {
 
--- architecture/src/main/java/com/kunminx/architecture/data/repository/DataResult.java
+++ architecture/src/main/java/com/kunminx/architecture/data/repository/DataResult.java
@@ -23,9 +23,9 @@ import com.kunminx.architecture.domain.manager.NetState;
 /**
  * TODO: 专用于数据层返回结果给 domain 层或 ViewModel 用，原因如下：
  * <p>
- * livedata 是专用于页面开发的、用于解决生命周期安全问题的组件，
- * 有时候数据并非一定是通过 livedata 来分发给页面，也可能是通过别的组件去通知给非页面的东西，
- * 这时候 repo 方法中内定通过 livedata 分发就不太合适，不如一开始就规定不在数据层通过 livedata 返回结果。
+ * liveData 是专用于页面开发的、用于解决生命周期安全问题的组件，
+ * 有时候数据并非一定是通过 liveData 来分发给页面，也可能是通过别的组件去通知给非页面的东西，
+ * 这时候 repo 方法中内定通过 liveData 分发就不太合适，不如一开始就规定不在数据层通过 liveData 返回结果。
  * <p>
  * Create by KunMinX at 2020/7/20
  */
