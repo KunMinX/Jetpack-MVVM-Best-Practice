2a759c417d8230b6bbc80ef372a683f1241a3e5a
KunMinX <kunminx@gmail.com>
2020-10-31 19:56:41 +0800

    update comment

--- app/src/main/java/com/kunminx/puremusic/App.java
+++ app/src/main/java/com/kunminx/puremusic/App.java
@@ -21,6 +21,15 @@ import com.kunminx.architecture.utils.Utils;
 import com.kunminx.puremusic.player.PlayerManager;
 
 /**
+ * TODO tip 1：需要为项目准备一个 Application 来继承 BaseApplication，
+ * 以便在 Activity/Fragment 中享用 Application 级作用域的 Callback-ViewModel
+ * <p>
+ * callback-ViewModel 的职责仅限于在 "跨页面通信" 的场景下，承担 "唯一可信源"，
+ * 所有跨页面的 "状态同步请求" 都交由本单例处理，并统一分发给所有订阅者页面。
+ * <p>
+ * 如果这样说还不理解的话，详见《LiveData 鲜为人知的 身世背景 和 独特使命》中结合实际场合 对"唯一可信源"本质的解析。
+ * https://xiaozhuanlan.com/topic/0168753249
+ * <p>
  * Create by KunMinX at 19/10/29
  */
 public class App extends BaseApplication {
