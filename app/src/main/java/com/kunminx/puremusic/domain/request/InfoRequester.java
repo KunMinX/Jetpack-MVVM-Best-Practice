b5f4b96661ea831a58287f55c2e805ae4714cf28
kunminx <kunminx@gmail.com>
2023-06-01 00:40:11 +0800

    update comment

--- app/src/main/java/com/kunminx/puremusic/domain/request/InfoRequester.java
+++ app/src/main/java/com/kunminx/puremusic/domain/request/InfoRequester.java
@@ -29,31 +29,46 @@ import java.util.List;
 
 /**
  * 信息列表 Request
- * <p>
- * TODO tip 1：基于 "单一职责原则"，应将 ViewModel 划分为 state-ViewModel 和 result-ViewModel，
- * result-ViewModel 职责仅限于 "消息分发" 场景承担 "可信源"。
- * <p>
+ *
+ * TODO tip 1：基于 "单一职责原则"，应将 Jetpack ViewModel 框架划分为 state-ViewModel 和 result-ViewModel，
+ * result-ViewModel 作为领域层组件，提取和继承 Jetpack ViewModel 框架中 "作用域管理" 的能力，
+ * 使业务实例能根据需要，被单个页面独享，或多个页面共享，
+ *
+ * result-ViewModel 作为领域层组件，职责仅限于 "业务逻辑处理 和 消息分发"，
+ * UI 逻辑和业务逻辑，本质区别在于，前者是数据的消费者，后者是数据的生产者，
+ * 数据总是来自领域层业务逻辑的处理，并单向回推至 UI 层，在 UI 层中响应数据的变化，
+ *
+ * 故此应根据上述介绍明确区分 UI 逻辑和业务逻辑，不要将 UI 逻辑理解为业务逻辑的一部分，
+ * result-ViewModel 中应当只关注数据的生成，而不关注数据的使用，
+ *
+ * 如这么说无体会，详见《再回首 页面搭档 Jetpack ViewModel》解析。
+ * https://xiaozhuanlan.com/topic/6018295743
+ *
+ * TODO tip 2：应顺应 "响应式编程"，做好 "单向数据流" 开发，
+ *
  * 常见消息分发场景包括：数据请求，页面间通信等，
- * 数据请求由 Requester 负责，页面通信由 Messenger 负责，
- * <p>
- * 所有事件都可交由 "可信源" 在内部决策和处理，并统一分发结果给所有订阅者页面。
- * <p>
+ * 数据请求由 Requester 负责，页面通信由 PageMessenger 负责，
+ *
+ * 所有事件都可交由 "鉴权中心" 在内部决策和处理，并统一分发结果给所有订阅者页面。
+ *
  * 如这么说无体会，详见《吃透 LiveData 本质，享用可靠消息鉴权机制》解析。
  * https://xiaozhuanlan.com/topic/6017825943
- * <p>
- * <p>
- * TODO tip 2：Requester 通常按业务划分
+ *
+ *
+ * TODO tip 3：Requester 通常按业务划分
  * 一个项目中通常存在多个 Requester 类，
  * 每个页面可根据业务需要持有多个不同 Requester 实例。
- * <p>
+ *
  * requester 职责仅限于 "业务逻辑处理" 和 "消息分发"，不建议在此处理 UI 逻辑，
- * UI 逻辑只适合在 Activity/Fragment 等视图控制器中完成，是 “数据驱动” 一部分，
- * 将来升级到 Jetpack Compose 更是如此。
- * <p>
- * 如这么说无体会，详见《这是一份 “架构模式” 自驾攻略》解析
- * https://xiaozhuanlan.com/topic/8204519736
- * <p>
- * <p>
+ * UI 逻辑和业务逻辑，本质区别在于，前者是数据的消费者，后者是数据的生产者，
+ * 数据总是来自领域层业务逻辑的处理，并单向回推至 UI 层，在 UI 层中响应数据的变化（也即处理 UI 逻辑），
+ * 换言之，UI 逻辑只适合在 Activity/Fragment 等视图控制器中编写，将来升级到 Jetpack Compose 更是如此。
+ *
+ *
+ * 如这么说无体会，详见《Jetpack MVVM 分层设计解析》解析
+ * https://xiaozhuanlan.com/topic/6741932805
+ *
+ *
  * Create by KunMinX at 19/11/2
  */
 public class InfoRequester extends ViewModel {
