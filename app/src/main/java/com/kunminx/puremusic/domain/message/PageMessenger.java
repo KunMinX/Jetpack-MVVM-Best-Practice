305b19ae7751292e504e5130de6a18d2e84e4d9e
kunminx <kunminx@gmail.com>
2023-06-05 23:18:35 +0800

    update comment

--- app/src/main/java/com/kunminx/puremusic/domain/message/PageMessenger.java
+++ app/src/main/java/com/kunminx/puremusic/domain/message/PageMessenger.java
@@ -5,32 +5,19 @@ import com.kunminx.puremusic.domain.event.Messages;
 
 /**
  * TODO:Note 2022.07.04
- * 可于领域层通过 MVI-Dispatcher 实现可靠的消息回推，
+ *  `
+ *  PageMessenger 是一个领域层组件，可用于 "跨页面通信" 场景，
+ *  比如跳转到 login 页面完成登录后，login 页面反过来通知其他页面刷新状态，
+ *
+ * PageMessenger 基于 MVI-Dispatcher 实现可靠的消息回推，
  * 通过消息队列、引用计数等设计，确保 "消息都能被消费，且只消费一次"，
  * 通过内聚设计，彻底杜绝 mutable 滥用等问题，
  *
  * 鉴于本项目场景难发挥 MVI-Dispatcher 潜能，故目前仅以改造 DownloadRequester 和 SharedViewModel 为例，
  * 通过对比 SharedViewModel 和 PageMessenger 易得，后者可简洁优雅实现可靠一致的消息分发，
  *
- * TODO tip: 于 PageMessenger 中统一鉴权和处理业务逻辑，并通过 sendResult 回推结果至表现层
  *
- * switch (event.eventId) {
- *    case Messages.EVENT_ADD_SLIDE_LISTENER:
- *        //... 业务逻辑处理
- *        //... 末端消息回推:
- *        // event.result.xxx = xxx;
- *        // sendResult(event);
- *        break;
- *    case Messages.EVENT_CLOSE_ACTIVITY_IF_ALLOWED:
- *        break;
- *    case Messages.EVENT_CLOSE_SLIDE_PANEL_IF_EXPANDED:
- *        break;
- *    case Messages.EVENT_OPEN_DRAWER:
- *        break;
- *  }
- *
- *
- * 具体可参见专为 MVI-Dispatcher 可信源编写 MVI 使用案例：
+ * 具体可参见专为 MVI-Dispatcher 编写的领域层案例：
  *
  * https://github.com/KunMinX/MVI-Dispatcher
  *
